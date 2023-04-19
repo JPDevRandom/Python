4f2905ae015630621fdafb87c9b28ade3179b2a7
JoãoPedro <131218441+JPDevRandom@users.noreply.github.com>
2023-04-18 22:18:46 -0300

    Delete calcularcorrente.py

--- calcularcorrente.py
+++
@@ -1,9 +0,0 @@
-while True:
-    idade = int(input("Digite a idade:"))
-
-    if idade > 18:
-        print("Pode dirigir magrão")
-    elif idade == 18:
-        print("Pode dirigir magrão")
-    else:
-        print("se fudeu magrao")
