73234036b7f825752eda6e2a4e0eb2eb8d1e4c7c
JPDevRandom <joaopedroalvesjardim@hotmail.com>
2023-04-18 21:51:44 -0300

    Initial commit

---
+++ calcularcorrente.py
@@ -0,0 +1,9 @@
+while True:
+    idade = int(input("Digite a idade:"))
+
+    if idade > 18:
+        print("Pode dirigir magrão")
+    elif idade == 18:
+        print("Pode dirigir magrão")
+    else:
+        print("se fudeu magrao")
---
+++ whiletest.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número: "))
+i = 1
+while i <= 10:
+    resultado = numero * i
+    print(numero, "x", i, "=", resultado)
+    i += 1
